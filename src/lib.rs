672f3f7a08a3a793f5e0064b8945f2f80aa601aa
Sean Batzel <romulus108@protonmail.com>
2022-02-04 13:56:33 -0500

    Marked FennelServerPacket fields as public.

--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,11 @@ pub use crate::rsa_tools::*;
 
 #[derive(Copy, Clone)]
 pub struct FennelServerPacket {
-    command: [u8; 1],
-    identity: [u8; 32],
-    fingerprint: [u8; 32],
-    message: [u8; 1024],
-    signature: [u8; 1024],
-    public_key: [u8; 1038],
-    recipient: [u8; 32],
+    pub command: [u8; 1],
+    pub identity: [u8; 32],
+    pub fingerprint: [u8; 32],
+    pub message: [u8; 1024],
+    pub signature: [u8; 1024],
+    pub public_key: [u8; 1038],
+    pub recipient: [u8; 32],
 }
