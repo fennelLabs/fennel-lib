563ff8423a152ddd4cff5c827f8665c18b19536d
Husky <huskydev@pm.me>
2022-02-07 14:45:37 -0500

    adding decrypt

--- src/aes_tools/mod.rs
+++ src/aes_tools/mod.rs
@@ -7,6 +7,44 @@ mod bench;
 #[cfg(test)]
 mod tests;
 
+struct AESCipher {
+    key: AesKey,
+    iv: Vec<u8>,
+}
+
+impl AESCipher {
+    /* fn new(key: AesKey, iv: Vec<u8>) -> AESCipher {
+        AESCipher {
+            key,
+            iv
+        }
+    } */
+
+    fn new() -> AESCipher {
+        AESCipher {
+            key: generate_key(),
+            iv: generate_buffer_32()
+                .try_into()
+                .expect("failed to convert into Vec"),
+        }
+    }
+}
+
+trait Cipher {
+    fn encrypt<T: AsRef<str>>(&mut self, plaintext: T) -> Vec<u8>;
+    fn decrypt<T: AsRef<Vec<u8>>>(&mut self, ciphertext: T) -> Vec<u8>;
+}
+
+impl Cipher for AESCipher {
+    fn encrypt<T: AsRef<str>>(&mut self, plaintext: T) -> Vec<u8> {
+        encrypt(&self.key, &mut self.iv, plaintext)
+    }
+
+    fn decrypt<T: AsRef<Vec<u8>>>(&mut self, ciphertext: T) -> Vec<u8> {
+        decrypt(&self.key, &mut self.iv, ciphertext)
+    }
+}
+
 pub fn generate_key() -> AesKey {
     let buf = generate_buffer_16(); // 128, 192, 256 bits or 16, 24, 32 bytes
     let aeskey = AesKey::new_encrypt(&buf).expect("failed to generate key");
@@ -15,19 +53,42 @@ pub fn generate_key() -> AesKey {
 
 pub fn generate_cipher() {}
 
-pub fn encrypt(message: String) -> Vec<u8> {
-    let length = calculate_resize(&message);
-    let mut m: Vec<u8> = message.into_bytes();
-    m.resize(length, 0);
-
+pub fn encrypt_gen_key_iv<T: AsRef<str>>(message: T) -> Vec<u8> {
     let aeskey = generate_key();
     let mut iv = generate_buffer_32(); //*b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F";
-    let mut outbuf = vec![0; length];
-    aes_ige(&m, &mut outbuf, &aeskey, &mut iv, Mode::Encrypt);
-    outbuf
+
+    encrypt(&aeskey, &mut iv, message)
 }
 
-pub fn decrypt() {}
+pub fn encrypt<T: AsRef<str>>(key: &AesKey, iv: &mut [u8], plaintext: T) -> Vec<u8> {
+    let plaintext_slice = plaintext.as_ref().as_bytes();
+    let pos = plaintext_slice.len();
+    let length = calculate_resize(pos);
+
+    let mut buffer = vec![0; length];
+    buffer[..pos].copy_from_slice(plaintext_slice);
+
+    //let (length, buffer) = normalize_input(plaintext.as_ref().as_bytes().to_vec());
+
+    let mut ciphertext = vec![0; length];
+    aes_ige(&buffer, &mut ciphertext, &key, iv, Mode::Encrypt);
+    ciphertext
+}
+
+pub fn decrypt<T: AsRef<Vec<u8>>>(key: &AesKey, iv: &mut [u8], ciphertext: T) -> Vec<u8> {
+    let ciphertext_slice = ciphertext.as_ref().as_slice();
+    let pos = ciphertext_slice.len();
+    let length = calculate_resize(pos);
+
+    let mut buffer = vec![0; length];
+    buffer[..pos].copy_from_slice(ciphertext_slice);
+
+    //let (length, buffer) = normalize_input(ciphertext.as_ref().to_owned());
+
+    let mut plaintext = vec![0; length];
+    aes_ige(&buffer, &mut plaintext, &key, iv, Mode::Decrypt);
+    plaintext
+}
 
 fn generate_buffer_16() -> [u8; 16] {
     let mut buf = [0; 16]; // 128, 192, 256 bits or 16, 24, 32 bytes
@@ -41,7 +102,17 @@ fn generate_buffer_32() -> [u8; 32] {
     buf
 }
 
-fn calculate_resize(message: &str) -> usize {
-    let length = message.len();
-    length + (16 - length % 16)
+fn calculate_resize(size: usize) -> usize {
+    size + (16 - size % 16)
+}
+
+
+/// currently not working properly
+fn normalize_input<T: Copy>(data: Vec<T>) -> (usize, Vec<T>) {
+    let pos = data.len();
+    let length = calculate_resize(pos);
+
+    let mut buffer: Vec<T> = Vec::with_capacity(length);
+    buffer[..pos].copy_from_slice(data.as_slice());
+    (length, buffer)
 }
--- src/aes_tools/tests.rs
+++ src/aes_tools/tests.rs
@@ -11,10 +11,33 @@ fn test_encrypt() {
     {
         \"id\": 1,
         \"name\": \"xyzab\"
+    }";
+
+    encrypt_gen_key_iv(message);
+
+    ()
+}
+
+#[test]
+fn test_message_is_same() {
+    let message = "
+    {
+        \"id\": 1,
+        \"name\": \"xyzab\"
     }"
     .to_string();
 
-    encrypt(message);
+    let mut cipher: AESCipher = AESCipher::new();
 
-    ()
+    let ciphertext: Vec<u8> = cipher.encrypt(message);
+    let plaintext = cipher.decrypt(ciphertext);
+
+    println!(
+        "{:?}",
+        String::from_utf8(plaintext).expect("Found invalid UTF-8")
+    );
+
+    //let decrypted = decrypt(encrypted);
+
+    assert_eq!(0, 1);
 }
