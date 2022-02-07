f0dfd03417e2fac2f8a7e04d5eaa5dfd27fd8a78
Husky <huskydev@pm.me>
2022-02-07 11:29:31 -0500

    adding tests

--- src/aes_tools/tests.rs
+++ src/aes_tools/tests.rs
@@ -1,1 +1,20 @@
+use crate::aes_tools::*;
 
+#[test]
+fn test_key_gen() {
+    generate_key();
+}
+
+#[test]
+fn test_encrypt() {
+    let message = "
+    {
+        \"id\": 1,
+        \"name\": \"xyzab\"
+    }"
+    .to_string();
+
+    encrypt(message);
+
+    ()
+}
