960bc4643c3a74795053a1bd4031f67dd1a64832
Husky <huskydev@pm.me>
2022-02-10 10:17:50 -0500

    cleaning up module

--- src/aes_tools/padding.rs
+++ src/aes_tools/padding.rs
@@ -1,19 +1,14 @@
 const PAD_BASE: usize = 16;
 const UTF8_SPACE: u8 = 32;
 
-pub fn calculate_resize(size: usize) -> usize {
-    size + (PAD_BASE - size % PAD_BASE)
-}
-
-/// ensures vector has a length that is a multiple of 16
-pub fn is_size_valid(data: &[u8]) -> bool {
-    data.len() % PAD_BASE == 0
-}
-
 /// will add padding to the end of a vector to ensure its size is a multiple of 16
 pub fn pad(data: &[u8], value: Option<u8>) -> Vec<u8> {
     let length = calculate_resize(data.len());
     let mut buffer = data.to_vec();
     buffer.resize(length, value.unwrap_or(UTF8_SPACE));
     buffer
+}
+
+fn calculate_resize(size: usize) -> usize {
+    size + (PAD_BASE - size % PAD_BASE)
 }
