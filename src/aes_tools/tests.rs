2d3fd0b833e9a7f1dd1d83bedaf0ec5e26a77f12
Husky <huskydev@pm.me>
2022-02-10 11:29:14 -0500

    using a random and larger message for testing

--- src/aes_tools/tests.rs
+++ src/aes_tools/tests.rs
@@ -7,11 +7,12 @@ fn test_key_gen_without_panic() {
 
 #[test]
 fn test_encrypt_without_panic() {
-    let message = "
-    {
-        \"id\": 1,
-        \"name\": \"xyzab\"
-    }";
+    let message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+    ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
     let cipher: AESCipher = AESCipher::new();
     cipher.encrypt(message);
@@ -20,11 +21,12 @@ fn test_encrypt_without_panic() {
 
 #[test]
 fn test_message_is_same() {
-    let message = "
-    {
-        \"id\": 1,
-        \"name\": \"xyzab\"
-    }";
+    let message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+    ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+    in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
     let cipher: AESCipher = AESCipher::new();
 
