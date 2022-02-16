8663e667d584d368c9bb37391d465ecea7cd149c
Andrew Plaza <aplaza@liquidthink.net>
2022-02-16 16:27:09 -0500

    fmt

--- src/fennel/mod.rs
+++ src/fennel/mod.rs
@@ -1,17 +1,11 @@
-use subxt::{
-    ClientBuilder,
-    DefaultConfig,
-    DefaultExtra,
-};
-
-
+use subxt::{ClientBuilder, DefaultConfig, DefaultExtra};
 
 /// To run this example, a local fennel node should be running.
 ///
 /// ```bash
 /// curl "https://github.com/paritytech/polkadot/releases/download/v0.9.13/polkadot" --output /usr/local/bin/polkadot --location
 /// fennel --dev --tmp
-/// 
+///
 /// # to fetch the metadata from a running dev node
 /// curl -sX POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"state_getMetadata", "id": 1}' localhost:9933 \
 ///                 | jq .result \
@@ -40,9 +34,11 @@ async fn fetch_storage() -> Result<(), Box<dyn std::error::Error>> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[tokio::test]
     async fn test_fetch() {
-        fetch_storage().await.expect("Storage should have been fetched");
+        fetch_storage()
+            .await
+            .expect("Storage should have been fetched");
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@
 
 pub mod aes_tools;
 pub mod database;
-pub mod rsa_tools;
 mod fennel;
+pub mod rsa_tools;
 
 use codec::{Decode, Encode};
 
