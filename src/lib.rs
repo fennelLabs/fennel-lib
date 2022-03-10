eae50157b32b3f42197a2a118f518399b3ee4143
Ed Hertzog <edhertzog@gmail.com>
2022-03-09 23:50:50 -0500

    Revert

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,6 @@ use codec::{Decode, Encode};
 pub use crate::aes_tools::*;
 pub use crate::database::*;
 pub use crate::dh_tools::*;
-pub use crate::fennel::*;
 pub use crate::rsa_tools::*;
 
 #[derive(Copy, Clone, Encode, Decode, Debug)]
