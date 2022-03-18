20c82fa06be1f0f709b40f5b705527cf75e95a40
Husky <huskydev@pm.me>
2022-03-18 17:32:38 -0400

    adding decode bdx

--- src/whiteflag/wf_codec/hexadecimal.rs
+++ src/whiteflag/wf_codec/hexadecimal.rs
@@ -1,5 +1,6 @@
-use super::constants::{HEXRADIX, QUADBIT};
+use super::constants::{BYTE, HEXRADIX, QUADBIT};
 
+/// Encodes a (hexa)decimal string into a binary buffer
 pub fn encodeBDX(mut data: String) -> Vec<u32> {
     if data.len() % 2 == 1 {
         data = data + "0";
@@ -20,52 +21,25 @@ pub fn encodeBDX(mut data: String) -> Vec<u32> {
 
 fn to_hex_digit(data: Option<char>) -> u32 {
     data.expect("out of bounds")
-        .clone()
         .to_digit(HEXRADIX as u32)
         .expect("failed to convert to digit")
 }
 
-/* /**
- * Encodes a (hexa)decimal string into a binary buffer
- * @since 1.1
- * @param bdxstr the (hexa)decimal string to encode
- * @return a binary buffer containing the encoded (hexa)decimal string
- */
-protected static final byte[] encodeBDX(final String bdxstr) {
-    /* Prepare string by removing prefix and adding trailing 0 */
-    String str = bdxstr;
-    if (bdxstr.length() % 2 == 1) str = str + "0";
-
-    /* Loop through hexadecimal string and take two chars at a time*/
-    final int strLength = str.length();
-    byte[] byteArray = new byte[strLength / 2];
-    for (int i = 0; i < strLength; i += 2) {
-        byteArray[i / 2] = (byte) ((Character.digit(str.charAt(i), HEXRADIX) << QUADBIT)
-                                    + Character.digit(str.charAt(i + 1), HEXRADIX));
-    }
-    return byteArray;
+fn from_hex_digit(data: u32) -> char {
+    std::char::from_digit(data, HEXRADIX as u32).expect("failed to convert to char")
 }
 
-/**
- * Decodes a binary buffer into a (hexa)decimal string
- * @since 1.1
- * @param buffer the binary buffer containing the binary encoded (hexa)decimals to decode
- * @param bitLength the buffer length, i.e. the number of bits in the buffer to decode
- * @return a (hexa)decimal string with the decoded data
- */
-protected static final String decodeBDX(final byte[] buffer, final int bitLength) {
-    StringBuilder str = new StringBuilder();
+/// Decodes a binary buffer into a (hexa)decimal string
+pub fn decodeBDX(buffer: Vec<u32>, bit_length: u8) -> String {
+    let mut hexadecimal_string: Vec<char> = Vec::new();
 
-    /* Loop through the bits in the binary buffer */
-    for (int bitIndex = 0; bitIndex < bitLength; bitIndex += BYTE) {
-        final int byteCursor = bitIndex / BYTE;
-
-        /* Add first half of byte to string */
-        str.append(Character.forDigit((buffer[byteCursor] >> QUADBIT) & 0xF, HEXRADIX));
-        if ((bitIndex + QUADBIT) < bitLength) {
-            /* Add second half of byte to string */
-            str.append(Character.forDigit((buffer[byteCursor] & 0xF), HEXRADIX));
+    for bit_index in (0..bit_length).step_by(BYTE) {
+        let byte_cursor = bit_index as usize / BYTE;
+        hexadecimal_string.push(from_hex_digit((buffer[byte_cursor] >> QUADBIT) & 0xF));
+        if (bit_index + (QUADBIT as u8)) < bit_length {
+            hexadecimal_string.push(from_hex_digit(buffer[byte_cursor] & 0xF));
         }
     }
-    return str.toString().toLowerCase();
-} */
+
+    hexadecimal_string.into_iter().collect()
+}
