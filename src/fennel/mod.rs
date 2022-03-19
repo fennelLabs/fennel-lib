281ad82c800f5b17f8c8070dea8992a88b16ea47
Ed Hertzog <edhertzog@gmail.com>
2022-03-18 20:40:08 -0400

    cargo fmt

--- src/fennel/mod.rs
+++ src/fennel/mod.rs
@@ -5,8 +5,8 @@ mod error;
 use std::sync::{Arc, Mutex};
 
 use rocksdb::DB;
-use subxt::{sp_core::sr25519::Pair, ClientBuilder, DefaultConfig, DefaultExtra, PairSigner};
 use sp_keyring::AccountKeyring;
+use subxt::{sp_core::sr25519::Pair, ClientBuilder, DefaultConfig, DefaultExtra, PairSigner};
 
 use crate::{get_identity_database_handle, get_message_database_handle};
 
