fee02fcb74fb3a2d7dc88fcfa62f04adda0c7a39
Ed Hertzog <edhertzog@gmail.com>
2022-03-18 20:54:14 -0400

    revert

--- src/fennel/mod.rs
+++ src/fennel/mod.rs
@@ -5,7 +5,7 @@ mod error;
 use std::sync::{Arc, Mutex};
 
 use rocksdb::DB;
-use sp_keyring::AccountKeyring;
+
 use subxt::{sp_core::sr25519::Pair, ClientBuilder, DefaultConfig, DefaultExtra, PairSigner};
 
 use crate::{get_identity_database_handle, get_message_database_handle};
