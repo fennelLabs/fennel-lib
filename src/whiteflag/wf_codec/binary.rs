e81ff155b6981a4171224c32128038511c46c0aa
Isaac Adams <huskydev@pm.me>
2022-04-03 22:57:16 -0400

    whiteflag v1 (#23)
    * Started stubbing out Whiteflag message processing.
    
    * Added passing tests.
    
    * More set up for auth messages, but still not passing tests.
    
    * Merged.
    
    * Passing tests for compiling auth messages.
    
    * Fixed a formatting failure.
    
    * brought in various encoding kinds from whiteflag
    
    * organizing and adding binary encoding method
    
    * adding decode
    
    * adding hexadecimal encoding function
    
    * adding decode bdx
    
    * fixing function names + using Vec<char> as base implementation
    
    * working on latlong implementation
    
    * adding common functions
    
    * making vec u8 instead of u32
    
    * adding latlong encode
    
    * moving to top
    
    * adding general encode method
    
    * adding general decoding method
    
    * shift_right tests are passing
    
    * shift left tests are passing
    
    * supporting backwards shift
    
    * adding Field and a definition
    
    * adding the remaining definitions
    
    * fixing test file
    
    * adding message type and from_code implementation
    
    * adding needed methods to Field
    
    * differentiating using a separate type between a field definition and a field
    
    * adding more functions to field and segment
    
    * fixing various build errors
    
    * obsolete lifetime declaration
    
    * fixing reference and ownership issues
    
    * making some progress with compile function
    
    * implementing clippy suggestions
    
    * nearly done with compile method
    
    * moving WhiteflagMesage into core mod
    
    * adding important common method and testing to ensure it works
    
    * integrating encoding into Field + using AsRef<str> + adding some missing methods
    
    * including message in module
    
    * vector needs to be filled with init values
    
    * generalizing string input
    
    * test builds
    
    * fixing issue with removing invalid hexadecimal characters and turning it into a common and tested function
    
    * fixing datetime encoding
    
    * removing look aheads and look behinds
    
    * allowing none for regex and making methods more flexible
    
    * removing old code
    
    * moving some functions into common
    
    * beginning to add field tests
    
    * fmt
    
    * better to accept &[u8]
    
    * adding more tests
    
    * removing 0x from hexadecimal strings
    
    * adding the remaining tests for field codec
    
    * properly calculating bit_length and propagating up through encoding process
    
    * preferring &[u8] as input type for u8 arrays
    
    * adding apend_bits method and ran the first successful whiteflag message encoding test
    
    * reorganizing tests
    
    * simplifying extraction of message_type
    
    * adding missing methods for decoding
    
    * wrapping up decode method
    
    * decode test passing
    
    * algorithm porting error
    
    * fmt
    
    * adding authentication decode test
    
    * encode authentication message test passes
    
    * Started on correcting a few warnings.
    
    * Local variable naming convention.
    
    * Because we're using an enum, we shouldn't need a default case.
    
    * cleaning up compiler warnings
    
    Co-authored-by: Sean Batzel <romulus108@protonmail.com>

---
+++ src/whiteflag/wf_codec/binary.rs
@@ -0,0 +1,44 @@
+use super::constants::BYTE;
+
+pub fn encode_binary<T: AsRef<str>>(binary_str: T) -> Vec<u8> {
+    let bit_length: usize = binary_str.as_ref().len();
+    let byte_length: usize = (bit_length / BYTE)
+        + (match bit_length % BYTE == 0 {
+            true => 0,
+            false => 1,
+        });
+    let mut buffer = vec![0; byte_length];
+
+    for bit_index in 0..bit_length {
+        if binary_str
+            .as_ref()
+            .chars()
+            .nth(bit_index)
+            .expect("something wrong")
+            == '1'
+        {
+            let byte_cursor: usize = bit_index / BYTE;
+            let bit_position: usize = bit_index % BYTE;
+            buffer[byte_cursor] |= 0x80 >> bit_position;
+        }
+    }
+
+    buffer
+}
+
+pub fn decode_binary(buffer: &[u8], bit_length: usize) -> String {
+    let mut data: Vec<char> = Vec::new();
+
+    for bit_index in 0..bit_length {
+        let byte_cursor: usize = bit_index / BYTE;
+        let bit_position: usize = bit_index % BYTE;
+
+        if (buffer[byte_cursor] >> (BYTE - bit_position - 1) & 1) == 1 {
+            data.push('1');
+        } else {
+            data.push('0');
+        }
+    }
+
+    data.into_iter().collect() //to lower?
+}
