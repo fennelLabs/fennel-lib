f859f8fdcd37b7bc157759f6debc8b9d98e8c18e
Isaac Adams <isaacdadams@pm.me>
2022-09-12 00:29:07 -0400

    `fmt`

--- src/database/tests.rs
+++ src/database/tests.rs
@@ -1,6 +1,6 @@
+use crate::database::*;
 use aes_tools::{AESCipher, FennelCipher};
 use dh_tools::{get_session_public_key, get_session_secret, get_shared_secret};
-use crate::database::*;
 use std::sync::Arc;
 
 #[test]
