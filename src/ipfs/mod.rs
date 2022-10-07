dcd2f013f134ff68b39f4bc77bbd2729d60de366
Sean Batzel <romulus108@protonmail.com>
2022-10-07 19:41:05 -0400

    Using a blocking client and a multipart form submission.

--- src/ipfs/mod.rs
+++ src/ipfs/mod.rs
@@ -1,50 +1,42 @@
 #[cfg(test)]
 mod tests;
-use reqwest::Body;
-use tokio::fs::File;
-use tokio_util::codec::{BytesCodec, FramedRead};
 
-pub async fn add_file(filename: &str) -> String {
+pub fn add_file(filename: &str) -> String {
     // https://docs.ipfs.io/reference/http/api/#api-v0-block-put
-    let client = reqwest::Client::new();
-
-    let file = File::open(filename).await.unwrap();
-    let body = Body::wrap_stream(FramedRead::new(file, BytesCodec::new()));
+    let client = reqwest::blocking::Client::new();
+    let form = reqwest::blocking::multipart::Form::new()
+        .file("data", filename)
+        .unwrap();
 
     let res = client.post("http://127.0.0.1:5001/api/v0/block/put?cid-codec=raw&mhtype=sha2-256&mhlen=-1&pin=false&allow-big-block=false")
-        .body(body)
+        .multipart(form)
         .send()
-        .await
         .unwrap()
         .text()
-        .await
         .unwrap();
     res
 }
 
-pub async fn get_file(cid: &str) -> String {
+pub fn get_file(cid: &str) -> String {
     // https://docs.ipfs.io/reference/http/api/#api-v0-block-get
-    let client = reqwest::Client::new();
+    let client = reqwest::blocking::Client::new();
 
     let res = client
         .post(format!("http://127.0.0.1:5001/api/v0/block/get?arg={}", cid).as_str())
         .send()
-        .await
         .unwrap()
         .text()
-        .await
         .unwrap();
     res
 }
 
-pub async fn del_file(cid: &str) -> bool {
+pub fn del_file(cid: &str) -> bool {
     // https://docs.ipfs.io/reference/http/api/#api-v0-block-rm
-    let client = reqwest::Client::new();
+    let client = reqwest::blocking::Client::new();
 
     let res = client
         .post(format!("http://127.0.0.1:5001/api/v0/block/rm?arg={}", cid).as_str())
         .send()
-        .await
         .unwrap()
         .status();
     res.is_success()
