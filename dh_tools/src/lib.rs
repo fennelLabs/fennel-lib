e90b842e930b924b645d9f83578b4984a7ecc4b9
Sean Batzel <romulus108@protonmail.com>
2023-07-16 12:53:21 -0400

    Updated x25519_dalek.

--- dh_tools/src/lib.rs
+++ dh_tools/src/lib.rs
@@ -2,19 +2,19 @@
 mod tests;
 
 use rand_core::OsRng;
-use x25519_dalek::{PublicKey, SharedSecret, StaticSecret};
+use x25519_dalek::{EphemeralSecret, PublicKey, SharedSecret};
 
 /// Generates a static secret for the current session, usable in Diffie-Hellman.
-pub fn get_session_secret() -> StaticSecret {
-    StaticSecret::new(OsRng)
+pub fn get_session_secret() -> EphemeralSecret {
+    EphemeralSecret::random_from_rng(OsRng)
 }
 
 /// Based on the user's static secret, generate a public key that can be used to generate encryption resources.
-pub fn get_session_public_key(secret: &StaticSecret) -> PublicKey {
+pub fn get_session_public_key(secret: &EphemeralSecret) -> PublicKey {
     PublicKey::from(secret)
 }
 
 /// Carries out the Diffie-Hellman handshake with another user's public key to generate a shared secret.
-pub fn get_shared_secret(my_secret: StaticSecret, their_public: &PublicKey) -> SharedSecret {
+pub fn get_shared_secret(my_secret: EphemeralSecret, their_public: &PublicKey) -> SharedSecret {
     my_secret.diffie_hellman(their_public)
 }
